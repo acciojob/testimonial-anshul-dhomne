c57f9ebf45d708dca73138b9dafca422fbcc00ad
anshul-dhomne <anshul.dhomnee@gmail.com>
2025-03-06 11:53:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-//your JS code here. If required.
+it("Checking tags", () => {
+    cy.get('h1').should('have.length', 1)
+    cy.get('div').should('have.length', 7) // updated from 3 to 7
+    cy.get('p').should('have.length', 6) // updated from 4 to 6
+});
